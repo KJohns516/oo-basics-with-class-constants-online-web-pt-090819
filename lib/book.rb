105095c0bd33c16e7beb2cf719d4e68a4cde7678
Kristan Johns <kristandjohns@gmail.com>
2019-09-28 00:52:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  GENRES = []
+  GENRES = ['keeps track of all brands'] do
 
   def initialize(title)
     @title = title
