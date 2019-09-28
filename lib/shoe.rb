28dbd42965a290b6982b3dcaa8ea2436e65c9bb6
Kristan Johns <kristandjohns@gmail.com>
2019-09-28 00:54:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-    BRAND = []
+    BRANDS = []
 
   def initialize(brands)
     @brands = brands
@@ -15,5 +15,5 @@ class Shoe
   
   def brand=(brand)
       @brand = brands
-      BRAND << brand
+      BRANDS << brand
 end
