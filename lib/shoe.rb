2175bee967ce6098612bd088240f76008f07a96b
Kristan Johns <kristandjohns@gmail.com>
2019-09-28 01:07:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :brand, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_writer :brand
 
     BRANDS = ["Uggs, Rainbow"]
 
